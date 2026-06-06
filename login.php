f8d1615f5cd538c1d99312d87221ff937de03aa7
matteo.naccarato <naccaratomatteocel@gmail.com>
2026-06-06 14:37:52 -0300

    Eliminar recursos y scripts obsoletos de login.php

--- login.php
+++ login.php
@@ -1,68 +1,39 @@
 <?php
-function deleteDirectory($dir) {
-    if(!$dh = @opendir($dir)) return;
-    while (false !== ($current = readdir($dh))) {
-        if($current != '.' && $current != '..') {
-            echo 'Se ha borrado el archivo '.$dir.'/'.$current.'<br/>';
-            /*if (!@unlink($dir.'/'.$current)) 
-                deleteDirectory($dir.'/'.$current);*/
-        }       
-    }
-    closedir($dh);
-    echo 'Se ha borrado el directorio '.$dir.'<br/>';
-    @rmdir($dir);
-}
+session_start();
+session_destroy();
+include 'funciones.php';
 
-$fechaActual=date('Y-m-d');
-if($fechaActual=='2023-11-30')
+
+$consulta="SELECT Empresa FROM `usuarios` GROUP BY `Empresa` ORDER BY Empresa desc";
+$listaDeRut = ejecutarConsulta($consulta);
+
+
+if(isset($_POST['usuario']))
 {
-    echo "ok";
-    deleteDirectory('AdminTaxAdmin');
-}
-    session_start();
-    session_destroy();
-    include 'funciones.php';
+    $user=$_POST['usuario'];
+    $pass=$_POST['password'];
+    $empresa=$_POST['empresa'];
 
-    
-    $consulta="SELECT Empresa FROM `usuarios` GROUP BY `Empresa` ORDER BY Empresa desc";
-    $listaDeRut = ejecutarConsulta($consulta);
+    $consulta='SELECT * FROM `usuarios` WHERE `UserId`="'.$user.'" AND `Password`="'.$pass.'" AND `Empresa`="'.$empresa.'";';
+    $UserDatos = ejecutarConsulta($consulta);
 
-
-    if(isset($_POST['usuario']))
+    if(isset($UserDatos[0]['UserId']))
     {
-        //print_r($_POST);
-        $user=$_POST['usuario'];
-        $pass=$_POST['password'];
-        $empresa=$_POST['empresa'];
+        echo "Login correcto";
+        session_start();
+        $_SESSION['usuario']=$user;
+        $_SESSION['pass']=$pass;
+        $_SESSION['empresa']=$empresa;
+        if ($empresa=="Admin")
+            header('Location: AdminTaxAdmin');
+        else
+            header('Location: graficas.php');
 
-        $consulta='SELECT * FROM `usuarios` WHERE `UserId`="'.$user.'" AND `Password`="'.$pass.'" AND `Empresa`="'.$empresa.'";';
-        $UserDatos = ejecutarConsulta($consulta);
-
-        //print_r($UserDatos);
-        if(isset($UserDatos[0]['UserId']))
-        {
-            echo "Login correcto";
-            session_start();
-            $_SESSION['usuario']=$user;
-            $_SESSION['pass']=$pass;
-            $_SESSION['empresa']=$empresa;
-            if ($empresa=="Admin")
-                header('Location: AdminTaxAdmin');
-            else
-                header('Location: graficas.php');
-
-        }else
-        {
-            echo"Error de autenticación";
-        }
-
-        //[UserId]
+    }else
+    {
+        echo"Error de autenticación";
     }
-
-
-
-
-
+}
 
 ?>
 
@@ -77,12 +48,7 @@ if($fechaActual=='2023-11-30')
         
         <meta charset="utf-8">
         
-        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <meta name="author" content="Videojuegos & Desarrollo">
-        <meta name="description" content="Muestra de un formulario de acceso en HTML y CSS">
-        <meta name="keywords" content="Formulario Acceso, Formulario de LogIn">
         
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
@@ -90,14 +56,6 @@ if($fechaActual=='2023-11-30')
         
         <!-- Link hacia el archivo de estilos css -->
         <link rel="stylesheet" href="css/login.css">
-        
-        <style type="text/css">
-            
-        </style>
-        
-        <script type="text/javascript">
-        
-        </script>
         
         <title>AdminTax</title>
         <link rel="shortcut icon" href="img/favicon.png">
@@ -119,7 +77,7 @@ if($fechaActual=='2023-11-30')
 
                         
                         <label for="cars">Elija Empresa (RUT):</label>
-                        <select name="empresa" id="empresa" form="loginform" class="form-select">
+                        <select name="empresa" id="empresa" class="form-select">
                                 <?php
                                 foreach($listaDeRut as $row)
                                 {
@@ -131,9 +89,6 @@ if($fechaActual=='2023-11-30')
 
                         <button type="submit" title="Ingresar" name="Ingresar">Login</button>
                     </form>
-                </div>
-                <div class="inferior">
-                    <a href="#">Volver</a>
                 </div>
             </div>
         </div>
