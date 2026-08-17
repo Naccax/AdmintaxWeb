2b468843bbc7fac48afb92d2e5580e042945fccf
matteo.naccarato <naccaratomatteocel@gmail.com>
2026-08-16 22:48:37 -0300

    Eliminacion de archivos no usados en AdminTaxAdmin

--- AdminTaxAdmin/funciones.php
+++ AdminTaxAdmin/funciones.php
@@ -15,91 +15,84 @@ $meces["12"]="Diciembre";
 $meces["13"]="Enero";
 
 function ejecutarConsulta($consulta)
-  {
-    $usuario = "root";
-    $password = "";
-    $servidor = "localhost";
-    $basededatos = "admintax";
+{
+  $usuario = "root";
+  $password = "";
+  $servidor = "localhost";
+  $basededatos = "admintax";
 
-    $conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
-    
-    // Selección del a base de datos a utilizar
-    $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+  $conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
   
-    // establecer y realizar consulta. guardamos en variable.
-    $sql = $consulta;
-    //echo $sql . '<br>';
-    $resultado = mysqli_query( $conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-    
-    $i=0;
-    $respuesta=[];
+  // Selección del a base de datos a utilizar
+  $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
 
-    //print_r( $resultado );
+  // establecer y realizar consulta. guardamos en variable.
+  $sql = $consulta;
+  $resultado = mysqli_query( $conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+  
+  $i=0;
+  $respuesta=[];
 
-    while ($columna = mysqli_fetch_array( $resultado ))
-    {
-      $respuesta[$i]=$columna;
-      //print_r($columna);
-      $i=$i+1;
-    }
-    
-    // cerrar conexión de base de datos
-    mysqli_close( $conexion );
-    
-    //print_r($resultado);
-    return $respuesta;
 
+  while ($columna = mysqli_fetch_array( $resultado ))
+  {
+    $respuesta[$i]=$columna;
+    $i=$i+1;
   }
   
-    /** Actual month last day **/
-    function _data_last_month_day() { 
-      $month = date('m');
-      $year = date('Y');
-      $day = date("d", mktime(0,0,0, $month+1, 0, $year));
- 
-      return date('Y-m-d', mktime(0,0,0, $month, $day, $year));
-  };
- 
-  /** Actual month first day **/
-  function _data_first_month_day() {
-      $month = date('m');
-      $year = date('Y');
-      return date('Y-m-d', mktime(0,0,0, $month, 1, $year));
-  }
-
-  $consulta="SELECT * FROM `chofer`";
-  $todosLosChoferes=ejecutarConsulta($consulta);
-  $choferes=[];
-
-  foreach($todosLosChoferes as $chofer)
-  {
-    $choferes[$chofer['id']]=$chofer;
-  }
-
-  $consulta="SELECT * FROM `empresa`";
-  $todosLasEmpresas=ejecutarConsulta($consulta);
-  $Empresas=[];
-
-  foreach($todosLasEmpresas as $chofer)
-  {
-    $Empresas[$chofer['id']]=$chofer;
-  }
-
-  function Insert($consulta){
-    
-    $usuario = "root";
-    $password = "";
-    $servidor = "localhost";
-    $basededatos = "admintax";
-    
-    $conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
-    
-    // Selección del a base de datos a utilizar
-    $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+  // cerrar conexión de base de datos
+  mysqli_close( $conexion );
   
-    // establecer y realizar consulta. guardamos en variable.
-    $sql = $consulta;
-    //echo $sql . '<br>';
-    $resultado = mysqli_query( $conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-    }
+  return $respuesta;
+}
+
+/** Actual month last day **/
+function _data_last_month_day() { 
+  $month = date('m');
+  $year = date('Y');
+  $day = date("d", mktime(0,0,0, $month+1, 0, $year));
+
+  return date('Y-m-d', mktime(0,0,0, $month, $day, $year));
+};
+
+/** Actual month first day **/
+function _data_first_month_day() {
+  $month = date('m');
+  $year = date('Y');
+  return date('Y-m-d', mktime(0,0,0, $month, 1, $year));
+}
+
+$consulta="SELECT * FROM `chofer`";
+$todosLosChoferes=ejecutarConsulta($consulta);
+$choferes=[];
+
+foreach($todosLosChoferes as $chofer)
+{
+  $choferes[$chofer['id']]=$chofer;
+}
+
+$consulta="SELECT * FROM `empresa`";
+$todosLasEmpresas=ejecutarConsulta($consulta);
+$Empresas=[];
+
+foreach($todosLasEmpresas as $chofer)
+{
+  $Empresas[$chofer['id']]=$chofer;
+}
+
+function Insert($consulta){
+  $usuario = "root";
+  $password = "";
+  $servidor = "localhost";
+  $basededatos = "admintax";
+  
+  $conexion = mysqli_connect( $servidor, $usuario, "" ) or die ("No se ha podido conectar al servidor de Base de datos");
+  
+  // Selección del a base de datos a utilizar
+  $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+
+  // establecer y realizar consulta. guardamos en variable.
+  $sql = $consulta;
+  $resultado = mysqli_query( $conexion, $sql ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+}
 ?>
